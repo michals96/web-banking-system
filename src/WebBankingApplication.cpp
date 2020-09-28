fa484333ef572dadbfa9aea6bb5ddec23015a311
michals96 <michal.stefaniuk9@gmail.com>
2020-09-29 00:13:33 +0200

    WBS-14 Add strings.xml file

--- src/WebBankingApplication.cpp
+++ src/WebBankingApplication.cpp
@@ -1,17 +1,16 @@
 #include "WebBankingApplication.h"
+#include <Wt/WApplication.h>
 #include <Wt/WText.h>
 
 WebBankingApplication::WebBankingApplication(const Wt::WEnvironment& env)
 	: Wt::WApplication(env),
 	  Wt::WContainerWidget()
 {
-
-
 	setTitle("Web banking system");
-
-	std::unique_ptr<Wt::WText> title(Wt::cpp14::make_unique<Wt::WText>("<h1>Web Banking System</h1>"));
-
+	messageResourceBundle().use(appRoot() + "strings");
 	useStyleSheet("css/FrontPage.css");
+	
+	std::unique_ptr<Wt::WText> title(Wt::cpp14::make_unique<Wt::WText>("<h1>Web Banking System</h1>"));
 
 	root()->addWidget(std::move(title));
 }
