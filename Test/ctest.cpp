0778c2afaa351b8f2eafc9f624db0712e70b606a
michals96 <michal.stefaniuk9@gmail.com>
2020-10-25 00:41:12 +0200

    WBS-24 Simple testing mock

--- Test/ctest.cpp
+++ Test/ctest.cpp
@@ -1,8 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "pch.h"
 
+//#ifndef _WIN32
+#include <stdio.h>
+#include <stdlib.h>
+#include"../include/TransactionsWidget.h"
+#include <Wt/WContainerWidget.h>
+
 int main(int argc, char** argv) {
+
+    TransactionsWidget widget;
+    std::string str = widget.currentDateTime();
+
     if (argc < 2) {
         printf("usage: test <argument>\n");
         printf("    If <argument> is 0, print SUCCESS. Otherwise print FAIL.\n");
@@ -13,8 +21,18 @@ int main(int argc, char** argv) {
         printf("SUCCESS\n");
         exit(0);
     }
+    if (atoi(argv[2]) == 0) {
+        if (str.length() != 0) {
+            printf("SUCCESS\n");
+        }
+        else {
+            printf("FAILED\n");
+        }
+        exit(0);
+    }
     else {
         printf("FAIL\n");
         exit(1);
     }
 }
+//#endif
