ecb055ed7fcf5e4217d12637299e1ec736dd5dde
michals96 <michal.stefaniuk9@gmail.com>
2020-10-25 20:31:03 +0100

    WBS-24 CTest wrong if else condition

--- Test/ctest.cpp
+++ Test/ctest.cpp
@@ -3,11 +3,8 @@
 #ifndef _WIN32
 #include <stdio.h>
 #include <stdlib.h>
-#include"../include/TransactionsWidget.h"
-#include <Wt/WContainerWidget.h>
 
 int main(int argc, char** argv) {
-
     if (argc < 2) {
         printf("usage: test <argument>\n");
         printf("    If <argument> is 0, print SUCCESS. Otherwise print FAIL.\n");
@@ -16,15 +13,6 @@ int main(int argc, char** argv) {
 
     if (atoi(argv[1]) == 0) {
         printf("SUCCESS\n");
-        exit(0);
-    }
-    if (atoi(argv[2]) == 0) {
-        if (str.length() != 0) {
-            printf("SUCCESS\n");
-        }
-        else {
-            printf("FAILED\n");
-        }
         exit(0);
     }
     else {
